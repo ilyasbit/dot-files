989f51dbe7bf13e8ee4a6ec717fd29f6af2226f4
Ilyas Fauzal Akbar <ilyasbit01@gmail.com>
2024-09-11 00:52:05 +0700

    add zsh, alacritty, starship

---
+++ dotfiles/.zshrc
@@ -0,0 +1,61 @@
+ZINIT_HOME=$HOME/.config/zinit/
+
+if [ ! -d $ZINIT_HOME ]; then
+  mkdir -p $ZINIT_HOME
+  git clone https://github.com/zdharma-continuum/zinit.git $ZINIT_HOME
+fi
+
+alias clear='clear -x'
+alias claer='clear -x'
+alias cls='clear -x'
+
+#source zinit in .zshrc
+source $ZINIT_HOME/zinit.zsh
+
+#add in zinit plugins
+
+# add zsh-syntax-highlighting
+zinit light zsh-users/zsh-syntax-highlighting
+
+# add zsh-autosuggestions
+zinit light zsh-users/zsh-autosuggestions
+
+# add zsh-completions
+zinit light zsh-users/zsh-completions
+
+# add fzf-tab
+zinit light "Aloxaf/fzf-tab"
+
+#add in snippets
+
+#autoload completion
+autoload -U compinit && compinit
+
+#history
+HISTFILE=~/.zsh_history
+HISTSIZE=5000
+SAVEHIST=$HISTSIZE
+HISTDUP=erase
+bindkey -e
+bindkey '^p' history-search-backward
+bindkey '^n' history-search-forward
+
+setopt appendhistory
+setopt sharehistory
+setopt hist_ignore_space
+setopt hist_ignore_all_dups
+setopt hist_save_no_dups
+setopt hist_ignore_dups
+setopt hist_find_no_dups
+
+#fzf integration
+source <(fzf --zsh)
+
+#Completion style
+zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
+zstyle ':completion:*' list-colors '${(s.:.)LS_COLORS}'
+zstyle ':completion:*' menu no
+zstyle ':fzf-tab:complete:*' fzf-preview 'ls --color $realpath'
+#aliases
+alias ls='ls --color'
+eval "$(starship init zsh)"
--- setup-environment.sh
+++ setup-environment.sh
@@ -1,1 +1,47 @@
+#!/bin/bash
 
+username=$1
+#check if username is provided
+if [ -z "$username" ]; then
+  echo "Please provide a username"
+  exit 1
+fi
+
+#check if user exists
+if ! id -u $username &>/dev/null; then
+  echo "User $username does not exist"
+  exit 1
+fi
+#check if user directory exists
+if [ ! -d /home/$username ]; then
+  echo "User directory /home/$username does not exist"
+  exit 1
+fi
+
+#update the package list
+apt update -y
+#download latest vscode package .deb
+wget -q https://go.microsoft.com/fwlink/?LinkID=760868 -O /tmp/vscode.deb
+#install required packages
+apt install -y jq yq curl tmux wget zsh unzip git ansible stow alacritty python3 python3-pip python3-venv apt-transport-https ca-certificates curl software-properties-common net-tools build-essential
+#install starship prompt
+sh -c "$(curl -fsSL https://starship.rs/install.sh)" -- -y
+#create font directory on home user
+mkdir -p /home/$username/.local/share/fonts
+#copy font to the font directory
+cp ./dotfiles/fonts/* /home/$username/.local/share/fonts
+#update the font cache
+fc-cache -f -v
+
+fzfurl=$(curl -s https://api.github.com/repos/junegunn/fzf/releases/latest | jq -r '.assets[] | select(.name | contains("linux_amd64")) | .browser_download_url')
+#download fzf
+wget -q $fzfurl -O /tmp/fzf.tar.gz
+#extract fzf
+tar -xzf /tmp/fzf.tar.gz -C /tmp
+#move fzf to /usr/local/bin
+mv /tmp/fzf /usr/local/bin
+#make fzf executable
+chmod +x /usr/local/bin/fzf
+
+#set zsh as default shell
+chsh -s $(which zsh) $username
